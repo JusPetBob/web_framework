3b4d36a7c1121011698735f12ad95802dd8b8a2c
JusPetBob <JusPetBob@gmail.com>
2026-06-27 19:33:01 +0200

    added path calls

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import core, os
-from flask import Flask,current_app,Response,render_template
+import core, os, json
+from flask import Flask,current_app,Response,render_template,send_file
 
 if __name__ == "__main__":
     import dotenv
@@ -9,9 +9,34 @@ app = Flask(__name__)
 print("Creating EventHandler in PID", os.getpid())
 app.eventhandler = core.EventHandler("recv")
 
+
 @app.route("/")
 def main():
     return render_template("main.html")
+
+
+@app.route("/api/storage/", defaults={"path": ""})
+@app.route("/api/storage/<path:path>")
+def storage(path:str):
+    if path == "":
+        return [i.removesuffix(":\\") for i in os.listdrives()]
+    
+    path = path.removesuffix("/")
+    p = path.split("/")
+    p[0] += ":"
+    
+    root_path = "\\".join(p).removesuffix("\\")
+    
+    if os.path.isfile(root_path):        
+        return send_file(root_path, "text")
+    
+    else:
+        root_path += "\\"
+            
+        l = [{"name":i, "path":"/api/storage/"+path+"/"+i, "type":"file" if os.path.isfile(root_path+i) else "folder"} for i in os.listdir(root_path)]
+        
+        return l
+
 
 @app.route("/api/events")
 def event_handler():
